c5e066ed9ad440088e5070d65701613f8245b00a
Anshuman Verma <anshumanverma@tychonsoftware.com>
2022-05-14 12:17:25 +0530

    Courtlist Panel

--- src/main/java/it/nexera/ris/web/beans/pages/dictionary/CourtListBean.java
+++ src/main/java/it/nexera/ris/web/beans/pages/dictionary/CourtListBean.java
@@ -2,15 +2,21 @@ package it.nexera.ris.web.beans.pages.dictionary;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.model.SelectItem;
 
+import it.nexera.ris.common.helpers.DateTimeHelper;
+import it.nexera.ris.persistence.view.RequestView;
 import it.nexera.ris.web.beans.EntityLazyListPageBean;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.HibernateException;
 import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
@@ -26,10 +32,21 @@ import it.nexera.ris.web.beans.EntityLazyInListEditPageBean;
 
 @ManagedBean(name = "courtListBean")
 @ViewScoped
+@Getter
+@Setter
 public class CourtListBean extends
         EntityLazyListPageBean<Court> implements Serializable {
 
     private static final long serialVersionUID = 3952135359518006317L;
+
+    private String name;
+
+    private String taxCode;
+
+    private Long selectedCityId;
+
+    private List<SelectItem> courtCities;
+
 //    private List<SelectItem> cities;
 //
 //    private Long selectedCityId;
@@ -59,14 +76,38 @@ public class CourtListBean extends
     public void onLoad() throws NumberFormatException, HibernateException,
             PersistenceBeanException, InstantiationException,
             IllegalAccessException, IOException {
-        this.loadList(Court.class, new Criterion[]
-                {Restrictions.or(Restrictions.eq("isDeleted", Boolean.FALSE),
-                        Restrictions.isNull("isDeleted"))}, new Order[]
-                {Order.asc("name")});
+
+        filterTableFromPanel();
 //        setCities(ComboboxHelper.fillList(City.class, Order.asc("description"), new Criterion[]{
 //                Restrictions.eq("external", Boolean.TRUE)
 //        }));
         
+    }
+
+    public void filterTableFromPanel() throws PersistenceBeanException, IllegalAccessException, InstantiationException {
+        List<Criterion> restrictions = new ArrayList<>();
+        if(!ValidationHelper.isNullOrEmpty(getName())){
+            restrictions.add(   Restrictions.ilike("name", getName(), MatchMode.ANYWHERE));
+        }
+        restrictions.add(Restrictions.or(Restrictions.eq("isDeleted", Boolean.FALSE),
+                Restrictions.isNull("isDeleted")));
+        List<Court> courts = DaoManager.load(Court.class, restrictions.toArray(new Criterion[0]));
+        List<Long> cityIds = new ArrayList<>();
+
+        for (Court court : courts) {
+            if (!ValidationHelper.isNullOrEmpty(court.getCity()) && !cityIds.contains(court.getCity().getId())) {
+                cityIds.add(court.getCity().getId());
+            }
+        }
+        if (!ValidationHelper.isNullOrEmpty(cityIds)) {
+            setCourtCities(ComboboxHelper.fillList(City.class,
+                    Order.asc("description"),
+                    new Criterion[]{Restrictions.isNotNull("province.id")
+                            , Restrictions.eq("external", Boolean.TRUE), Restrictions.in("id", cityIds)}, Boolean.FALSE));
+        }
+        this.loadList(Court.class, restrictions.toArray(new Criterion[0]), new Order[]
+                {Order.asc("name")});
+
     }
 
 //    public List<SelectItem> getCities() {
@@ -84,4 +125,5 @@ public class CourtListBean extends
 //    public void setSelectedCityId(Long selectedCityId) {
 //        this.selectedCityId = selectedCityId;
 //    }
+
 }
